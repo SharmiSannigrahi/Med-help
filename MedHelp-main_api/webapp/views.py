b0d9ef245f6c7ad7b9178aa470123f0355a95672
SharmiSannigrahi <sannigrahisharmi2@gmail.com>
2025-06-30 11:54:08 +0530

    Add files via upload

---
+++ MedHelp-main_api/webapp/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, HttpResponse # To render websites
+import json # To handle json objects
+import requests # To fetch api
+
+# Api urls
+url_breastcancer = 'http://127.0.0.1:8090/breastcancer_prediction'
+url_diabetes = 'http://127.0.0.1:8080/diabetes_prediction'
+
+# Views functions
+def index(request):
+    return render(request, 'templates/index.html')
+
+def about(request):
+    return render(request, 'templates/about.html')
+
+def bmi(request):
+    return render(request, 'templates/bmi.html')
+
+def services(request):
+    return HttpResponse("This is Services Page")
+
+def contact(request):
+    return HttpResponse("This is Contact Page")
+
+def diabetes(request):
+  pg = request.POST.get('pgdb', '')  # Get the posted values or empty strings if not provided
+  gl = request.POST.get('gldb', '')
+  bm = request.POST.get('bmidb', '')
+  age = request.POST.get('agedb', '')
+
+  if request.method == 'POST':
+    input = {                        # Creating input dictionary
+        'Pregnancies' : pg,
+        'Glucose' : gl,
+        'BMI' : bm,
+        'Age' : age
+    }
+    inp = json.dumps(input)         # Creating json input
+    response = requests.post(url_diabetes, data=inp) # Getting response
+    result = response.text
+  else:
+    result = None
+
+  # Pass the form data and result to the template
+  return render(request, 'templates/diabetes.html', {'result': result, 'pg': pg, 'gl': gl, 'bm': bm, 'age': age})
+
+def breastcancer(request):
+    rm = request.POST.get('rm', '')
+    pm = request.POST.get('pm', '')
+    am = request.POST.get('am', '')
+    cm = request.POST.get('cm', '')
+    cnm = request.POST.get('cnm', '')
+    cpm = request.POST.get('cpm', '')
+
+    if request.method == 'POST':
+        input = {
+            'radius': rm,
+            'perimeter': pm,
+            'area': am,
+            'compactness': cm,
+            'concativity': cnm,
+            'concave_points': cpm
+        }
+        inp = json.dumps(input)
+        response = requests.post(url_breastcancer, data=inp)
+        result = response.text
+    else:
+        result = None
+
+    # Pass the form data and result to the template
+    return render(
+        request, 'templates/breastcancer.html', {
+            'result': result,
+            'rm': rm,
+            'pm': pm,
+            'am': am,
+            'cm': cm,
+            'cnm': cnm,
+            'cpm': cpm
+        })
---
+++ breastcancer_ml_api/main.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from sklearn.preprocessing import StandardScaler
+from joblib import load
+import json
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('breastcancer.csv')
+X = dataset.drop(columns = 'Outcome', axis = 1)
+columns_to_drop = ['tm', 'sm', 'sym', 'fdm', 'rse', 'tse', 'pse', 'ase', 'sse', 'cse', 'cnse', 'cpse', 'symse', 'fdse', 'rw', 'tw', 'pw', 'aw', 'sw', 'cw', 'cnw', 'cpw', 'symw', 'fdw']
+X = X.drop(columns=columns_to_drop)
+
+scaler = StandardScaler()
+scaler.fit(X)
+
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class model_input(BaseModel):
+    
+    radius : float
+    perimeter : float
+    area : float
+    compactness : float
+    concativity : float
+    concave_points : float
+    
+
+# loading the saved model
+diabetes_model = load('breastcancer_model.joblib')
+
+
+@app.post('/breastcancer_prediction')
+def diabetes_pred(input_parameters : model_input):
+    
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
+    
+    rm = input_dictionary['radius']
+    pm = input_dictionary['perimeter']
+    am = input_dictionary['area']
+    cm = input_dictionary['compactness']
+    cnm = input_dictionary['concativity']
+    cpm = input_dictionary['concave_points']
+
+
+    input_list = scaler.transform([[rm, pm, am, cm, cnm, cpm]])
+    
+    prediction = diabetes_model.predict(input_list)
+    
+    if prediction[0] == 0:
+        return 'Benign'
+    
+    else:
+        return 'Melignant'
+
+
